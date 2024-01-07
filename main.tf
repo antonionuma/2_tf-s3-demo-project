0db23af9ac4d7a9d0d22c440236f520c1930faea
antonionuma <cruces81@gmail.com>
2024-01-07 17:33:32 -0500

    update code

--- main.tf
+++ main.tf
@@ -26,20 +26,30 @@ resource "aws_s3_bucket" "test" {
   #acl    = "private"
 
 
-  server_side_encryption_configuration {
+ /* server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         kms_master_key_id = aws_kms_key.this.arn
         sse_algorithm     = "aws:kms"
       }
     }
-  }
+  }*/
 
   lifecycle {
     prevent_destroy = false
   }
 
   tags = local.common_tags
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = var.name
+   rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.this.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
